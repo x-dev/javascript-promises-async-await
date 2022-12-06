a8e06f38d3b5e743656279724633a425a45266e0
Christian Mayer <christian.mayer@outlook.com>
2022-12-06 11:11:53 -0500

    module 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-export function fetchMovies() { }
+import { fetchWithTimeout } from './services';
+
+const movies = require('./data/movies.json');
+
+export function fetchMovies() {
+
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
+
+moviePromise.then((results) => { console.log(results) });
